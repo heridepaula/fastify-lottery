124647ffe490ea38e869107cf8e8a299bc2ed606
Herivelton Gonzaga de Paula <heridepaula@gmail.com>
2020-11-11 00:44:32 -0300

    Ajust envirorment name

--- cronjobs/keep-alive.js
+++ cronjobs/keep-alive.js
@@ -4,6 +4,6 @@ const CronJob = require('cron').CronJob
 
 module.exports = async () => {
   (new CronJob('* 20/20 * * * *', async () => {
-    await got(`${process.env.PRICE_WATCHER_URL}/api/v1/keep-alive`)
+    await got(`${process.env.FASTIFY_LOTTERY_URL}/api/v1/keep-alive`)
   }, null, true, 'America/Los_Angeles')).start();
 }
